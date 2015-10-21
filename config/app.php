ed72f2a718488c9eb2401cd18312e2dc2ac84f00
Diego Vieira <diegorvieira17@gmail.com>
2015-10-21 16:28:25 -0200

    Corrigindo erro

--- config/app.php
+++ config/app.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'url' => 'http://localhost',
+    'url' => 'http://estoquectrl.herokuapp.com/',
 
     /*
     |--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'SomeRandomString'),
+    'key' => env('APP_KEY', 'Ee5hWmDMPBBufbV0Cq9SRFUbOBQfOAFx'),
 
     'cipher' => 'AES-256-CBC',
 
